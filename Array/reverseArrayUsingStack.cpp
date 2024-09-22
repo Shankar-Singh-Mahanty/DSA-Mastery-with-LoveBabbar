8ea5a6ffc794306ccb1f49ce25491ab2a03b96f3
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-09-22 18:43:48 +0530

    initial commit

---
+++ Array/reverseArrayUsingStack.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void reverseArrayUsingStack(int arr[], int size){
+    stack<int> stack;
+
+    for(int i = 0; i < size; i++) {
+        stack.push(arr[i]);
+    }
+
+    for(int i = 0; i < size; i++) {
+        arr[i] = stack.top();
+        stack.pop();
+    }
+}
+
+int main(){
+    int arr[] = {2, 4, 6, 8, 10};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    reverseArrayUsingStack(arr, size);
+
+    for(auto x : arr) cout << x << " ";
+    
+    return 0;
+}
